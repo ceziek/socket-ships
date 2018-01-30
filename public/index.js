229c00e2e16a903fa0ca6289fd6a501165173227
cezary.bajera <c.bajera@gmail.com>
2018-01-30 17:13:05 +0100

    working version

--- public/index.js
+++ public/index.js
@@ -4,13 +4,19 @@ import Game from './Game.js';
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const socket = io('localhost:3000');
+const socket = io('http://fuku.nazwa.pl:3000');
 const state = new State();
 
 socket.on('connect', () => {
     const game = new Game(ctx, state, socket);
     game.start();
 });
+
+window.upgrade = function() {
+    console.log('upgrade');
+    socket.emit('upgrade');
+};
+
 
 
 
